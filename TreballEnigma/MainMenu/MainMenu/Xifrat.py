66896c3478fcaf9b9567e4881f1aaf9951fb3325
TreballEnigmaEnti <laia.garcia@estudiant.enti.cat>
2025-12-14 22:57:30 +0100

    Corregir errores
    Subir el código sin la función descifrar otra vez por problemas técnicos en la función

--- TreballEnigma/MainMenu/MainMenu/Xifrat.py
+++ TreballEnigma/MainMenu/MainMenu/Xifrat.py
@@ -20,6 +20,7 @@ def xifrar():
     contadorListas = 0
     listaNuevaPosicionFinal = []
 
+
     for i in range(0, len(TextoNuevoDescifrar), separador):
         separados = TextoNuevoDescifrar[i:i + separador]
         TextoSeparado.append(separados)
@@ -79,60 +80,5 @@ def escribeTexto(listadeletras):
     archivo.write(listadeletras)
 
 
-def Posicionar_a_inicial(llista, lletra_inicial):
-    while llista[0] != lletra_inicial:
-        RotarLista(llista)
-
-def escribeTextoDesxifrat(listadeletras):
-    archivo = open('desxifrat.txt', 'w')
-    archivo.write(listadeletras)
-    print(f"[OK] Missatge desxifrat a 'desxifrat.txt'")
-
-
 def desxifrar():
-    try:
-        with open('Text.txt', 'r') as archivo:
-            TextoXifrat = archivo.read().strip()
-    except FileNotFoundError:
-        print("[ERROR] Fitxer Text.txt no trobat.")
-        return
-
-    TextoNouDesxifrar = TextoXifrat.replace(" ", "").upper()
-    
-    if not TextoNouDesxifrar:
-        print("El fitxer Text.txt està buit o no conté lletres a desxifrar.")
-        return
-
-    print("\n Configuració Inicial per Desxifrar ")
-    posicio_inicial_rotor2 = input("Introdueix la posició inicial del rotor 1 (A-Z): ").upper()
-    posicio_inicial_rotor3 = input("Introdueix la posició inicial del rotor 2 (A-Z): ").upper()
-
-    Posicionar_a_inicial(lista2, posicio_inicial_rotor2)
-    Posicionar_a_inicial(lista3, posicio_inicial_rotor3)
-
-    
-    listaNouDesxifrat = []
-    contadorListas = 0
-
-    for letra in TextoNouDesxifrar:
-        RotarLista(lista2)
-        contadorListas += 1
-
-        if contadorListas == len(listaABC):
-            RotarLista(lista3)
-            contadorListas = 0
-            RotarLista(lista2)
-
-        PosicioLetra = lista3.index(letra) 
-        LetraIntermedia = listaABC[PosicioLetra] 
-        
-        PosicioLetra2 = lista2.index(LetraIntermedia) 
-        LetraFinal = listaABC[PosicioLetra2]
-        
-        listaNouDesxifrat.append(LetraFinal)
-        
-    
-    TextoDesxifratFinal = "".join(listaNouDesxifrat)
-    
-    escribeTextoDesxifrat(TextoDesxifratFinal)
-    print("Text original (desxifrat):", TextoDesxifratFinal)
+    print("Funcio desxifrar en construccio")
