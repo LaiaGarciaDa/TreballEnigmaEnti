e453663b3ed159886b9dbfd338e4ce3dc88efe46
TreballEnigmaEnti <laia.garcia@estudiant.enti.cat>
2025-12-14 22:42:25 +0100

    Eliminar y subir
    Eliminar replicas, subir funciones de descifrado i eliminar líneas innecesarias

--- TreballEnigma/MainMenu/MainMenu/MainMenu.py
+++ TreballEnigma/MainMenu/MainMenu/MainMenu.py
@@ -3,10 +3,12 @@ import Xifrat
 
 def mostrar_pantalla():
     print("Enigma")
+    print("-------------")
     print("1. Notches")
     print("2. Xifrar")
     print("3. Borrar")
-    print("4. Sortir")
+    print("4. Desxifrar")
+    print("5. Sortir")
 
 
 def opcio_1():
@@ -22,6 +24,10 @@ def opcio_3():
     archivo.write(" ")
     print("Text esborrat correctament")
 
+
+def opcio_4():
+    Xifrat.desxifrar()
+
 while True:
     mostrar_pantalla()
     opcio = input("Selecciona que vols fer (1-3)")
@@ -33,6 +39,8 @@ while True:
     elif(opcio == '3'):
         opcio_3()
     elif(opcio == '4'):
+        opcio_4()
+    elif(opcio == '5'):
         print("Sortir")
         break
     else:
